1f955b0d672ea777997e3b62b4732a8cb02bb056
Cody <cody.tian@icloud.com>
2021-11-07 14:15:56 -0800

    add spaceship, fix edge-touch-back-and-forth error

--- particle.js
+++ particle.js
@@ -2,11 +2,13 @@
 class Particle {
     constructor(x, y) {
         this.pos = new p5.Vector(x, y)
-        this.vel = p5.Vector.random2D()
+        this.vel = new p5.Vector(1, -1)
         this.acc = new p5.Vector(0, 0)
-        this.r = 5 // radius
-        this.maxForce = 0.01 // our maximum force
-        this.maxSpeed = 5 // our maximum speed
+        this.sheet = loadImage("spaceship-48x48.png")
+        this.maxForce = 0.1 // our maximum force
+        this.maxSpeed = 2 // our maximum speed
+        this.r = 5 // even though sheets don't have a radius, we still have
+        // a radius because we can be a ball or our sprite.
 
         this.wanderAngle = 0 // this is the meaning of our point on our circle.
 
@@ -17,17 +19,33 @@ class Particle {
         this.path = [this.currentPath]
     }
 
-    show() {
-        noStroke()
-        fill(0, 0, 100)
-        circle(this.pos.x, this.pos.y, this.r*2)
+    show_spaceship() {
+        // instead of drawing circles, we should draw our spaceship. Wee!!
+        // noStroke()
+        // fill(0, 0, 100)
+        // circle(this.pos.x, this.pos.y, this.r*2)
+        imageMode(CENTER)
+        push()
+        translate(this.pos.x, this.pos.y)
+        rotate(this.vel.heading() + PI/2)
+        image(this.sheet, 0, 0)
+        noFill()
+        stroke(0, 0, 100)
+        rectMode(CENTER)
+        strokeWeight(0.1)
+        rect(0, 0, 48, 48)
+        pop()
+    }
 
-        // we can draw our path!
+    show_ball() {
+        circle(this.pos.x, this.pos.y, this.r)
+    }
+
+    show_path() {
         noFill()
         stroke(0, 0, 100, 20)
         strokeWeight(2)
         for (let path of this.path) {
-
             beginShape()
             for (let v of path) {
                 vertex(v.x, v.y)
@@ -53,21 +71,87 @@ class Particle {
         this.acc.add(f)
     }
 
+
+    kiwi_wander() {
+        let wanderPoint = this.vel.copy()
+        const WANDER_DISTANCE = 100
+        const WANDER_RADIUS = 50
+        // we set the magnitude before we add the position!
+        wanderPoint.setMag(WANDER_DISTANCE)
+        wanderPoint.add(this.pos)
+
+        strokeWeight(1)
+        stroke(0, 0, 100, 40)
+        line(this.pos.x, this.pos.y, wanderPoint.x, wanderPoint.y)
+        // this.arrowLine(this.pos, wanderPoint)
+
+        // area of wanderPoint circle
+        noFill()
+        stroke(0, 0, 100, 40)
+        circle(wanderPoint.x, wanderPoint.y, WANDER_RADIUS*2)
+
+        // point at center of wander circle
+        stroke(200, 100, 100) // blue
+        strokeWeight(3)
+        point(wanderPoint.x, wanderPoint.y)
+
+        // have our wanderPoint be somewhere on the circle
+        let totalAngle = this.vel.heading() + this.wanderAngle
+        let x = WANDER_RADIUS*cos(totalAngle)
+        let y = WANDER_RADIUS*sin(totalAngle)
+
+        // line between blue point (center of wander circle) and green point
+        // point along wander circumference
+        strokeWeight(1)
+        stroke(0, 0, 100, 40)
+        line(wanderPoint.x, wanderPoint.y, wanderPoint.x+x, wanderPoint.y+y)
+        // this.arrowLine(wanderPoint, new p5.Vector(
+        //     wanderPoint.x+x, wanderPoint.y+y))
+
+
+        wanderPoint.add(new p5.Vector(x, y))
+
+        strokeWeight(3)
+        stroke(90, 100, 100) // green
+        point(wanderPoint.x, wanderPoint.y)
+
+        // another line from our center to the wanderPoint, now offset by angle
+        strokeWeight(1)
+        stroke(0, 0, 100, 40)
+        line(this.pos.x, this.pos.y, wanderPoint.x, wanderPoint.y)
+        // this.arrowLine(this.pos, wanderPoint)
+
+        // a circle showing the range
+        circle(wanderPoint.x, wanderPoint.y, WANDER_RADIUS)
+
+        // position vector from our center to the wanderPoint
+        let steeringForce = wanderPoint.sub(this.pos)
+        steeringForce.setMag(this.maxForce)
+        this.applyForce(steeringForce)
+
+        this.wanderAngle += random(-1, 1)*0.5
+    }
+
     edges() {
 
         // did we hit an edge? ...
         let hitEdge = false
         // x's
 
+        let r = 24 // the airplane is 48x48. We're in ImageMode Center right
+        // now.
+
         if (this.pos.x + this.r > width) { // right
             this.pos.x = this.r // we want to wrap around
             // in any of these, we should set hitEdge to true. Here...
             hitEdge = true
+            console.log(this.pos.x)
         }
         else if (this.pos.x - this.r < 0) { // left
             this.pos.x = width - this.r
             // ...here...
             hitEdge = true
+            console.log(this.pos.x)
         }
 
         // y's
@@ -77,11 +161,13 @@ class Particle {
             this.pos.y = this.r
             // ...here...
             hitEdge = true
+            console.log(this.pos.y)
         }
         else if (this.pos.y - this.r < 0) { // top
             this.pos.y = height - this.r
             // ...and here...
             hitEdge = true
+            console.log(this.pos.y)
         }
         // ...and if so...
         if (hitEdge) {
@@ -95,17 +181,17 @@ class Particle {
 
     // makes us wander around the room. This wander is deprecated because it
     // gives a twitchy wander, which we don't want.
-    // wander() {
-    //     let force = p5.Vector.random2D()
-    //     return force.limit(this.maxForce)
-    // }
+    deprecated_wander() {
+        let force = p5.Vector.random2D()
+        return force.limit(this.maxForce)
+    }
 
     // makes us actually wander all over the room
-    actual_wander() {
+    wander() {
         // what is our wander point?
         let wanderDistance = 100
         let wanderRadius = 50
-        let wanderPoint = this.vel
+        let wanderPoint = this.vel.copy()
         wanderPoint.setMag(wanderDistance)
         wanderPoint.add(this.pos)
 
@@ -122,8 +208,8 @@ class Particle {
         // on our circle based on this.wanderAngle!
         // we need to find the offset between our current wander point and
         // the point that we're applying our steering force to.
-        let x = wanderRadius*cos(this.wanderAngle)
-        let y = wanderRadius*sin(this.wanderAngle)
+        let x = wanderRadius*cos(this.wanderAngle+this.vel.heading())
+        let y = wanderRadius*sin(this.wanderAngle+this.vel.heading())
         // and now we can add the existing PVector(x, y) to our wander point.
         wanderPoint.add(new p5.Vector(x, y))
         // we can also draw a point there.
@@ -136,7 +222,7 @@ class Particle {
         line(this.pos.x, this.pos.y, wanderPoint.x, wanderPoint.y)
 
         // we also need to add a random bit to this.wanderAngle.
-        this.wanderAngle += random(-0.1, 0.1)
+        this.wanderAngle += random(-0.3, 0.3)
 
         return p5.Vector.sub(wanderPoint, this.pos).limit(this.maxForce)
     }
--- sketch.js
+++ sketch.js
@@ -10,6 +10,7 @@ version comments
 
  */
 let font
+let particles
 let p
 let gravity
 
@@ -20,14 +21,28 @@ function preload() {
 function setup() {
     createCanvas(640, 360)
     colorMode(HSB, 360, 100, 100, 100)
-    p = new Particle(random(width), random(height))
+    particles = []
+    // for (let i = 0; i < 100; i++) {
+    //     let p = new Particle(random(width), random(height))
+    //     particles.push(p)
+    // }
+    p = new Particle(width/2, height/2)
     gravity = new p5.Vector(0, 0.1)
+    // frameRate(10)
 }
 
 function draw() {
     background(234, 34, 24)
-    p.show()
+    // for (let p of particles) {
+    //     p.update()
+    //     p.show_ball()
+    //     p.applyForce(p.wander())
+    //     p.edges()
+    // }
     p.update()
-    p.applyForce(p.actual_wander())
+    p.show_ball()
+    p.show_path()
+    p.applyForce(p.wander())
+    // p.kiwi_wander()
     p.edges()
 }
